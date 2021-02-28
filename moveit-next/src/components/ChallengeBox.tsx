155a34d6ea3250ad6eb637a04d087ba84e43738b
Stephani Henrique <56699770+StephaniHenrique@users.noreply.github.com>
2021-02-28 18:47:08 -0300

    Update ChallengeBox.tsx

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -45,7 +45,7 @@ export function ChallengeBox() {
                 </div>
             ) : (
                     <div className={styles.challengeNotActive}>
-                        <strong>Finalize um cilo para receber um desafio</strong>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
                         <p>
                             <img src="icons/level-up.svg" />
                     Avance um level completando um desafio
